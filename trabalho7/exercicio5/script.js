ded2e0df675b863700205809b40b925bde2b52c2
Pedro <pedroleale@gmail.com>
2025-03-07 17:00:45 -0300

    trab7

--- trabalho7/exercicio5/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const helloWorldElement = document.getElementById('hello-world');
-    helloWorldElement.textContent = 'Hello, World!';
-});
